e27908766f1f1eb325b4d77b819003320455122c
Emil Haugbergsmyr <emil@raeven.net>
2019-11-16 23:23:29 +0100

    Changed the structure to make use of Generic Types.

--- SAPI/Client.cs
+++ SAPI/Client.cs
@@ -2,7 +2,7 @@
 using SAPI.Models;
 using System.Collections.Generic;
 using RestSharp;
-using System.Management.Automation;
+using SAPI.Errors;
 
 namespace SAPI
 {
@@ -13,9 +13,8 @@ namespace SAPI
         public bool IsAuthenticated { get; private set; }
 
         public TokenResponse Token { get; set; }
-        // 65D38d873945s2T
 
-        public Client(PSCredential credential)
+        public Client(string email, string password)
         {
             RestClient = new RestClient("https://bokning.shoppisostersund.se/");
             var request = new RestRequest(Method.POST);
@@ -23,8 +22,8 @@ namespace SAPI
             request.RequestFormat = DataFormat.Json;
             request.AddJsonBody(new TokenRequest
             {
-                Email = credential.UserName,
-                Password = credential.GetNetworkCredential().Password,
+                Email = email,
+                Password = password,
                 GrantType = "password",
                 Scope = "profile email"
             });
@@ -41,37 +40,32 @@ namespace SAPI
             }
         }
 
-        public List<ReservationResponse> Reservations(int skip = 0, int take = 25)
+        public (List<T>, int) GetObjects<T>(int skip, int take)
         {
             if (!IsAuthenticated)
                 throw new NotAuthenticatedException();
 
             var request = new RestRequest(Method.GET);
             request.RequestFormat = DataFormat.Json;
-            request.Resource = "api/api/v1/reservation?skip={skip}&sort=reservationNo&take={take}";
-            request.AddHeader("Authorization", "Bearer " + Token.Token);
-            request.AddUrlSegment("skip", skip);
-            request.AddUrlSegment("take", take);
-
-            IRestResponse<List<ReservationResponse>> response = RestClient.Execute<List<ReservationResponse>>(request);
+            switch (typeof(T).Name) {
+                case "Reservation":
+                    request.Resource = "api/api/v1/reservation?skip={skip}&sort=reservationNo&take={take}";
+                    break;
+                case "Product":
+                    request.Resource = "api/api/v1/product?skip={skip}&sort=-productNo&take={take}";
+                    break;
+                case "Transaction":
+                    request.Resource = "api/api/v1/customer/transactions?skip={skip}&sort=-time&query=&take={take}";
+                    break;
+                default:
+                    throw new InvalidObjectTypeException();
+            }
             
-            return response.Data;
-        }
-
-        public (List<ProductResponse>, int) Products(int skip = 0, int take = 25)
-        {
-            if (!IsAuthenticated)
-                throw new NotAuthenticatedException();
-
-            var request = new RestRequest(Method.GET);
-            request.RequestFormat = DataFormat.Json;
-            request.Resource = "api/api/v1/product?skip={skip}&sort=-productNo&take={take}";
             request.AddHeader("Authorization", "Bearer " + Token.Token);
             request.AddUrlSegment("skip", skip);
             request.AddUrlSegment("take", take);
 
-            List<Product> pList = new List<Product>();
-            IRestResponse<List<ProductResponse>> response = RestClient.Execute<List<ProductResponse>>(request);
+            IRestResponse<List<T>> response = RestClient.Execute<List<T>>(request);
 
             int TotalCount = 0;
             foreach (Parameter hdr in response.Headers)
@@ -79,52 +73,37 @@ namespace SAPI
                 if (hdr.Name.Equals("X-TotalCount"))
                     TotalCount = Int32.Parse(hdr.Value.ToString());
             }
-            /*
-            foreach (ReservationResponse rr in response.Data)
-            {
-                rList.Add(new Reservation
-                {
-                    Nr = rr.ReservationNo,
-                    Cubicle = rr.reservableObject.Name,
-                    Customer = rr.Customer.Name,
-                    From = rr.ReservedFrom,
-                    To = rr.ReservedTo,
-                });
-            }
-            */
 
             return (response.Data, TotalCount);
         }
 
-        public (List<ProductResponse>, int) AllProducts()
-        {
-            if (!IsAuthenticated)
-                throw new NotAuthenticatedException();
-
-            (List<ProductResponse> pList, int totalItems) = Products(0, 200);
-            while (pList.Count - 1 < totalItems)
-            {
-                (List<ProductResponse> tPList, int tTotalItems) = Products(pList.Count - 1, 200);
-                pList.AddRange(tPList);
-            }
-            return (pList, totalItems);
-        }
-
-        public (List<Product>, int) SearchProducts(string query, int skip, int take)
+        public (List<T>, int) GetObjects<T>(int skip, int take, string query) where T : Base, new()
         {
             if (!IsAuthenticated)
                 throw new NotAuthenticatedException();
 
             var request = new RestRequest(Method.GET);
             request.RequestFormat = DataFormat.Json;
-            request.Resource = "api/api/v1/product?skip={skip}&sort=-productNo&take={take}&query={query}";
+            switch (typeof(T).Name)
+            {
+                case "Reservation":
+                    request.Resource = "api/api/v1/reservation?skip={skip}&sort=reservationNo&query={query}&take={take}";
+                    break;
+                case "Product":
+                    request.Resource = "api/api/v1/product?skip={skip}&sort=-productNo&query={query}&take={take}";
+                    break;
+                case "Transaction":
+                    request.Resource = "api/api/v1/customer/transactions?skip={skip}&sort=-time&query={query}&take={take}";
+                    break;
+                default:
+                    throw new InvalidObjectTypeException();
+            }
+
             request.AddHeader("Authorization", "Bearer " + Token.Token);
             request.AddUrlSegment("skip", skip);
             request.AddUrlSegment("take", take);
-            request.AddUrlSegment("query", query);
 
-            List<Product> pList = new List<Product>();
-            IRestResponse<List<ProductResponse>> response = RestClient.Execute<List<ProductResponse>>(request);
+            IRestResponse<List<T>> response = RestClient.Execute<List<T>>(request);
 
             int TotalCount = 0;
             foreach (Parameter hdr in response.Headers)
@@ -133,41 +112,21 @@ namespace SAPI
                     TotalCount = Int32.Parse(hdr.Value.ToString());
             }
 
-            foreach (ProductResponse pr in response.Data)
-            {
-                if (pr.Price > 0)
-                {
-                    pList.Add(new Product
-                    {
-                        ProductNumber = pr.ProductNumber,
-                        Name = pr.Name,
-                        Price = pr.Price,
-                        ProductCode = pr.ProductCode,
-                        Status = StatusFromBool(pr.ReadOnly),
-                        Cubicle = pr.ActiveReservableObjectName,
-                        Total = SumFromSales(pr.Sales),
-                    });
-                }
-            }
-            return (pList, TotalCount);
+            return (response.Data, TotalCount);
         }
 
-        private string StatusFromBool(bool value)
+        public (List<T>, int) GetAllObjects<T>()
         {
-            if (value)
-                return "Sold";
-            else
-                return "New";
-        }
+            if (!IsAuthenticated)
+                throw new NotAuthenticatedException();
 
-        private float SumFromSales(List<Sale> sales)
-        {
-            float total = 0;
-            foreach (Sale s in sales)
+            (List<T> pList, int totalItems) = GetObjects<T>(0, 200);
+            while (pList.Count - 1 < totalItems)
             {
-                total += s.Amount;
+                (List<T> tPList, int tTotalItems) = GetObjects<T>(pList.Count - 1, 200);
+                pList.AddRange(tPList);
             }
-            return total;
+            return (pList, totalItems);
         }
     }
 }

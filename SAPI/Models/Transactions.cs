f78d852d07894c7ea45ee2a7b364eb3b85272e3a
Emil Haugbergsmyr <emil@raeven.net>
2019-11-16 23:20:18 +0100

    Added support for requesting transactions.

---
+++ SAPI/Models/Transactions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPI.Models
+{
+    public class Transaction : Base
+    {
+        [JsonProperty(PropertyName = "time")]
+        public DateTime Time { get; set; }
+
+        [JsonProperty(PropertyName = "reservableObject")]
+        public ReservableObject ReservableObject { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
+
+        [JsonProperty(PropertyName = "total")]
+        public float Total { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+    }
+}

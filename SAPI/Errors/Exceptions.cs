b0a6b46aabc0d2f9b15bb4599a18556f92bff310
Emil Haugbergsmyr <emil@raeven.net>
2019-11-16 23:19:50 +0100

    Moved the exceptions to its own folder.

---
+++ SAPI/Errors/Exceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPI.Errors
+{
+    class NotAuthenticatedException : Exception
+    {
+        public NotAuthenticatedException()
+        {
+
+        }
+    }
+
+    class InvalidCredentialsException : Exception
+    {
+        public InvalidCredentialsException()
+        {
+
+        }
+    }
+
+    class InvalidObjectTypeException : Exception
+    {
+        public InvalidObjectTypeException()
+        { 
+        
+        }
+    }
+}
